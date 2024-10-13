9e6718c533e0c63f12143533e25f04683c83d09b
omerc7 <omerc7@gmail.com>
2024-10-13 18:31:29 +0300

    test

---
+++ tools/hello_world_tool/tool_def.py
@@ -0,0 +1,26 @@
+from . import main
+
+import inspect
+
+
+from kubiya_sdk.tools.models import Tool, Arg, FileSpec
+from kubiya_sdk.tools.registry import tool_registry
+
+hello_tool = Tool(
+    name="get_weather",
+    type="docker",
+    image="python:3.12",
+    description="Prints hello {name}!",
+    args=[Arg(name="name", description="name to say hello to", required=True)],
+    content="""
+python /tmp/main.py "{{ .name }}"
+""",
+    with_files=[
+        FileSpec(
+            destination="/tmp/main.py",
+            content=inspect.getsource(main),
+        ),
+    ],
+)
+
+tool_registry.register("hello", hello_tool)
