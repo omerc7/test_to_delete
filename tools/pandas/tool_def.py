fa727bcd31ad85ecbfa5dade8e66c68162e2fcad
omerc7 <omerc7@gmail.com>
2024-10-14 20:02:07 +0300

    add req

--- tools/pandas/tool_def.py
+++ tools/pandas/tool_def.py
@@ -13,12 +13,24 @@ pandas = Tool(
     description="does pandas things {name}!",
     args=[Arg(name="name", description="name for pandas", required=True)],
     content="""
+curl -LsSf https://astral.sh/uv/install.sh | sh > /dev/null 2>&1
+. $HOME/.cargo/env
+
+uv venv > /dev/null 2>&1
+. .venv/bin/activate > /dev/null 2>&1
+
+uv pip install -r /tmp/requirements.txt > /dev/null 2>&1
+
 python /tmp/main.py "{{ .name }}"
 """,
     with_files=[
         FileSpec(
             destination="/tmp/main.py",
             content=inspect.getsource(main),
+        ),
+        FileSpec(
+            destination="/tmp/requirements.txt",
+            content="pandas==2.2.3",
         ),
     ],
 )
