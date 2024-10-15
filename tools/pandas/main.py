d09672be4b27cab1bb2ef8faadb8e454cac7c38a
omerc7 <omerc7@gmail.com>
2024-10-15 18:23:06 +0300

    add hello llm key

--- tools/pandas/main.py
+++ tools/pandas/main.py
@@ -15,9 +15,7 @@ from kubiya_sdk.tools import function_tool
 
 @function_tool(
     description="Prints pandas {name}!",
-    requirements="""
-pandas==2.2.3
-""",
+    requirements=["pandas==2.2.3"],
 )
 def test_123(
     name: str,
@@ -34,3 +32,37 @@ def test_123(
     )
 
     print(df)
+
+
+@function_tool(
+    description="Prints pandas {name}!",
+    requirements=["litellm==1.49.4"],
+    env=["LLM_API_KEY", "LLM_BASE_URL"],
+)
+def litellm_hello_world(
+    name: str,
+):
+    import os
+    import litellm
+
+    llm_key = os.environ["LLM_API_KEY"]
+    llm_base_url = os.environ["LLM_BASE_URL"]
+
+    try:
+        response = litellm.completion(
+            model="ggpt-4o",
+            api_key=llm_key,
+            base_url=llm_base_url,
+            messages=[
+                {
+                    "content": f"Your task it to great people in a random movie star way, you must say which movie star you choose",
+                    "role": "system",
+                },
+                {"content": f"My name is {name}, greet me!", "role": "user"},
+            ],
+        )
+    except Exception as e:
+        print(e)
+        return
+
+    print(response.choices[0].message.content)
